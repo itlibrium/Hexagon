cdd104ac5e3e983e2251d5899b2614aa4623c7d1
Marcin Markowski <31286082+technites-pl@users.noreply.github.com>
2017-11-08 15:04:22 +0100

    Integration with SimpleInjector: stateless domain logic registration.

---
+++ ITLibrium.Hexagon.Domain.SimpleInjector/SimpleInjectorDomainExtensions.cs
@@ -0,0 +1,18 @@
+﻿using ITLibrium.Hexagon.Domain.Meta;
+using ITLibrium.Hexagon.SimpleInjector.Registration;
+using ITLibrium.Hexagon.SimpleInjector.Selectors;
+
+namespace ITLibrium.Hexagon.Domain.SimpleInjector
+{
+    public static class SimpleInjectorDomainExtensions
+    {
+        public static ITypesSelection IncludeDomainStatelessLogic(this ITypesSelection typesSelection)
+        {
+            return typesSelection.Include(Components.AnnotatedBy(
+                typeof(DomainServiceAttribute),
+                typeof(PolicyAttribute),
+                typeof(RepositoryAttribute),
+                typeof(FactoryAttribute)));
+        }
+    }
+}
